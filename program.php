3c3ee80b6747a899980c788adbe7a9e964a414a9
Daniel Ikechukwu Omede <danielomede@hotmail.com>
2023-06-14 13:32:42 +0100

    Create program.php
    Program screen html template

---
+++ program.php
@@ -0,0 +1,433 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> Hykr - The Hiking app </title>
+    
+    
+    
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/splide-core.min.css">
+    <!-- Material Design Theming -->
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.orange-indigo.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
+  
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta name="keywords" content="" />
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="fav.png">
+    <link rel="icon" type="image/png" href="fav.png">
+    
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+    <style>
+    
+    .footer {
+        position: absolute;
+        left: 0;
+        bottom: 50px; /* Height of the bottom navigation menu */
+        width: 100%;
+        height: 60px;
+        background-color: #f5f5f5;
+        text-align: center;
+    }
+    /*  Text colours */
+    .text-red {
+            color: #ee6962;
+        }
+        
+    .text-yellow {
+            color: #fcca48;
+        }
+        
+    .text-green {
+            color: #8ececa;
+        }
+        
+    .text-purp {
+            color: #9282f0;
+        }
+        
+    .text-cyan {
+            color: #00ffff;
+        }    
+    /*  Text colours */
+    
+    .bg-red {
+        background: #ee6962 !important;
+    }
+    
+    .bg-yellow {
+        background: #fcca48;
+    }
+    
+    .bg-green {
+        background: #8ececa;
+    }
+    
+    .bg-purp {
+        background: #9282f0;
+    }
+    
+    .bg-cyan {
+        background: #00ffff;
+        color: white;
+    
+    }
+    
+    .text-blue {
+        color: #38b6ff;
+    }
+    
+    .text-purple {
+        color: #ff4aff;
+    }
+
+/*  Border colours */
+    .border-red {
+            border: 1px #ee6962 solid;
+            color: #ee6962;
+        }
+        
+    .border-yellow {
+            border: 1px #fcca48 solid;        
+            color: #fcca48;
+        }
+        
+    .border-green {
+            border: 1px #8ececa solid;        
+            color: #8ececa;
+        }
+        
+    .border-purp {
+            border: 1px #9282f0 solid;        
+            color: #9282f0;
+        }
+        
+    .border-cyan {
+            border: 1px #00ffff solid;        
+            color: #00ffff;
+        }    
+        
+    .border-dark {
+            border: 1px #000000 solid;        
+            color: #000000;
+        }        
+    /*  Border colours */
+
+
+.bg-gray {
+    background: #d8e1ed;
+}
+    
+    .post-footer {
+            padding-top: 0px;
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+
+        }
+        
+        .post-footer .item {
+           flex: 1;
+           display: flex;
+           flex-direction: row;
+           text-align: center;
+        }
+        
+        .post-footer .item a {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row;
+        }
+        
+        .post-footer .item button {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row;
+        }
+        
+        .post-footer .item .icon-wrapper {
+            
+            width: 48px;
+            height: 48px;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 10px;
+            color: #fff;
+            font-size: 24px;
+            margin-bottom: 14px;
+        }
+    
+    .listview>li footer {
+        display: flex;
+    }
+    
+        body {
+            color: white;
+        }
+        
+        .card {
+            --bs-card-bg: #161616;
+        }
+        
+        a {
+            text-decoration: none !important;
+        }
+        
+        .text-watcher {
+            color: #f99907 !important;
+        }
+        
+        .bg-watcher {
+            background: #f99907 !important;
+            
+        }
+        
+        .trans-grey-card {
+            background: rgba(220,220,233, 0.2) !important;
+            color: white;
+        }
+        
+        .trans-green-card {
+            background: rgba(108, 243 , 127, 20%) !important;
+        }
+        
+        .form-control {
+            color: black !important;
+        }
+    
+        .bg-kasuwa-red {
+            background: #cf455c;
+        }
+        
+        .form-group.boxed .form-control {
+            background: rgba(220,220,233, 0.2) !important;
+        }
+        
+        
+    .text-red {
+        color: #cf455c;
+    }
+        
+    .nav-tabs.lined .nav-item .nav-link.active {
+        color: #ee6962;
+        border-bottom-color: #06060603 !important;
+    } 
+        
+    
+        
+    .listview {
+        border-radius: 10px;
+        /* background: rgba(220,220,233, 0.2) !important; */
+    }    
+    
+    .appBottomMenu {
+        background: transparent !important;
+    }
+        
+    .nav-tabs.lined .nav-item .nox.active {
+        border: 2px #000000 !important;
+    }
+    
+    .exampleBox {
+            border: 0px;
+    }
+    
+    .appHeader {
+        
+    }
+    
+    .modal-header {
+        background: transparent !important;
+        border-bottom: none !important;
+    }
+    
+    .extraHeader {
+        background: #ffffff !important;
+    }
+    
+    .modal {
+       /*background: #1c1c1c !important;*/
+        --bs-modal-bg: #ffffff !important;
+    }
+    
+    .e-dark {
+        background: #161616 !important;
+    }
+    
+    .e-grey {
+        background: #1c1c1c;
+    }
+        
+    .e-input {
+        background: #333333;
+    }    
+    
+    .listview> li {
+        flex-direction: column !important ;
+        align-items: center;
+    }
+    
+    .listview>li:after{
+      background : #1c1c1c;
+      height: 10px;
+      
+    }
+    
+    .image-listview>li:after {
+        left: 0;
+    }
+
+    li {
+        background: #ffffff;
+    }
+        
+    .image-listview > li a.item::after {
+        display: none;
+    }
+    
+    
+    #map-canvas{
+    
+    position: absolute;
+    top: 100px;
+    left: 0;
+    bottom: 100px;
+    right: 0;
+    }
+
+    /* Set the size of the div element that contains the map */
+    #map {
+    /*  height: 600px;  The height is 400 pixels */
+    /*  width: 100%; /* The width is the width of the web page */
+     height: 100%;
+      position: absolute; 
+      top: 0; 
+      bottom: -200px; 
+      left: 0; 
+      right: 0; 
+      z-index: 0;
+    }
+    
+    #container {
+  z-index: 100;
+  position: relative;
+}
+
+ .image-listview> li>footer .item {
+        display: flex;
+    }
+
+.wallet-card-section:before {
+    top: -56px;
+    height: 300px;
+}
+
+.icon-box{
+    
+    height:  40px;
+    width: 40px;
+    margin-top: 10px;
+    border-radius: 10px;
+    display: flex;
+    flex-wrap: nowrap;
+    align-content: flex-start;
+
+}
+
+.wallet-card {
+    bottom: 0px;
+}
+.wallet-card .wallet-footer {
+    border-top: 0px;
+}
+    </style>
+    <script type="module" src=""></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            checkUserInformation();
+        });
+    
+        function checkUserInformation() {
+            // Make an AJAX request to the server-side PHP script
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'check_user_info.php', true);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    // Display the message returned by the PHP script
+                    document.getElementById('message').innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
+        }
+    </script>
+</head>
+<body onload="initMap()" class="text-dark"  style="background: #ffffff" >
+
+    <!-- App Header -->
+    <div class="appHeader bg-white no-border">
+        <div class="left text-dark">
+            <span><a onclick="window.history.back()" class="headerButton goBack" > <i class="material-icons text-dark">chevron_left</i></a></span>   
+        </div>
+        <div class="center text-dark"> 
+            <strong>Program</strong>                           
+        </div>
+        <div class="right">
+            <a data-toggle="modal" class="me-1 mb-1" data-target="#sidebarPanel" style=" background: transparent !important;">
+                <i class="material-icons-outlined">more_vert</i>
+            </a>
+        </div> 
+    </div>
+    <!-- * App Header --> 
+    <div id="appCapsule">
+        <div class="section mt-2">
+            <figure>
+                <img src="programs/duration.png" alt="image" class="imaged img-fluid">
+            </figure>
+            <br>
+            <h3 class="text-center">Program title</h3>
+            <p class="text-secondary">
+                Vivamus venenatis at purus at varius. Nam pharetra, magna et interdum dignissim, purus risus ullamcorper
+                ipsum, et pharetra turpis ex vel orci.
+            </p>
+            
+            <div class="wallet-card">
+                <!-- Balance -->
+                
+                <!-- * Balance -->
+                <!-- Wallet Footer -->
+                <div class="wallet-footer">
+                    <div class="item" style="display:  flex;">
+                        <div>
+                            <small class="text-secondary">Burns</small>
+                            <strong><h4>180 kcal</h4></strong>    
+                        </div>
+                    </div>
+                    
+                    <div class="item" style="display:  flex;">
+                        <div>
+                            <small class="text-secondary">Duration</small>
+                            <strong><h4>25 mins</h4></strong>    
+                        </div>
+                    </div>
+                </div>
+                <!-- * Wallet Footer -->
+            </div>
+        </div>
+    </div>
+<!-- ///////////// Js Files ////////////////////  -->
+    <!-- Jquery -->
+    <script src="assets/js/lib/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap-->
+    <script src="assets/js/lib/popper.min.js"></script>
+    <script src="assets/js/lib/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/splide/splide.min.js"></script>    
+</body>    
