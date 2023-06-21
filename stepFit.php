7f9359c1fbbc1ffa3661e2d98d2631dcbfd91997
Daniel Ikechukwu Omede <danielomede@hotmail.com>
2023-06-21 14:58:44 +0100

    Create stepFit.php
    In this optimized code, the following changes were made:
    
    The script for generating the session code was included.
    The startStepCounter function was renamed to startStepFitProgram.
    The session code is displayed in a separate section.
    The updateStepCountDisplay and updateCaloriesBurned functions were combined into a single updateStepCountDisplay function to update both the step count and calories burned.
    The saveStepCountAndCaloriesToLocalStorage function was created to save the session data (step count and calories burned) to localStorage.
    The step count and calories burned are updated in localStorage whenever there is a change.
    The step count and calories burned are retrieved from localStorage when the program starts.
    The stopStepCounter function was renamed to stopStepFitProgram.
    
    Added the userReference variable to store the user reference and made it global so that it can be accessed in multiple functions.
    
    Passed the caloriesBurned parameter to the saveStepCountAndCaloriesToLocalStorage function when calling it from updateCaloriesBurned function.
    
    Moved the updateProgramMemberData function definition above the insertProgramMember function definition for better organization.
    
    Added the timestamp parameter to the insertProgramMember function and included it in the data sent in the POST request.
    
    Fixed a typo in the saveStepCountAndCaloriesToLocalStorage function where CaloriesBurned should be caloriesBurned when updating the program member data.

---
+++ stepFit.php
@@ -0,0 +1,193 @@
+<script>
+  let stepCount = 0;
+  let isCounting = false;
+  const stepThreshold = 20; // Adjusted acceleration magnitude threshold
+  let sessionCode;
+  let userReference; // Variable to store the user reference
+
+  // Function to generate a random session code
+  function generateSessionCode() {
+    const sessionCodeLength = 10;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let code = '';
+    for (let i = 0; i < sessionCodeLength; i++) {
+      code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+  }
+
+  function startStepFitProgram() {
+    isCounting = true;
+    sessionCode = generateSessionCode();
+    document.getElementById('viewSteps').style.display = 'block';
+    
+    // Insert user's reference, stepfit reference, timestamp, and session code into the 'program_members' table
+    userReference = '<?= $reference ?>'; // Replace with the actual user reference
+    const stepFitReference = '7777777'; // Replace with the actual stepfit reference
+    const timestamp = new Date().toISOString(); // Replace with the actual timestamp
+    insertProgramMember(userReference, stepFitReference, sessionCode, timestamp);
+
+    // Retrieve stepCount and caloriesBurned from localStorage if available
+    const storedData = JSON.parse(localStorage.getItem('stepFitData'));
+    if (storedData && storedData.sessionCode === sessionCode) {
+      stepCount = storedData.stepCount;
+      updateStepCountDisplay();
+      updateCaloriesBurned();
+      console.log('Step count and calories burned retrieved from localStorage');
+    }
+  }
+
+  function stopStepFitProgram() {
+    isCounting = false;
+    saveStepCountAndCaloriesToLocalStorage();
+  }
+
+  function updateStepCountDisplay() {
+    document.getElementById('stepCountDisplay').innerText = stepCount;
+    saveStepCountAndCaloriesToLocalStorage();
+  }
+
+  function updateCaloriesBurned() {
+    const gender = '<?= $gender ?>'; // Replace with the user's gender ('male' or 'female')
+    const height = <?= $height ?>; // Replace with the user's height in centimeters
+    const age = <?= $age ?>; // Replace with the user's age in years
+    const weight = <?= $weightInKg ?>; // Replace with the user's weight in kilograms
+
+    const caloriesBurned = convertStepsToCalories(stepCount, gender, height, age, weight);
+    document.getElementById('viewstepCalories').innerHTML = caloriesBurned.toFixed(2); // Display calories with 2 decimal places
+    saveStepCountAndCaloriesToLocalStorage(caloriesBurned);
+  }
+
+  function saveStepCountAndCaloriesToLocalStorage(caloriesBurned) {
+    const data = {
+      sessionCode: sessionCode,
+      stepCount: stepCount,
+      caloriesBurned: caloriesBurned
+    };
+    localStorage.setItem('stepFitData', JSON.stringify(data));
+    // Additionally, update the step count and calories burned in the database
+    updateProgramMemberData(userReference, sessionCode, stepCount, caloriesBurned);
+  }
+
+  window.addEventListener("devicemotion", (event) => {
+    console.log(`${event.acceleration.x} m/s2`);
+
+    if (isCounting) {
+      const acceleration = event.acceleration || event.accelerationIncludingGravity;
+      const accelerationMagnitude = Math.sqrt(
+        Math.pow(acceleration.x, 2) +
+        Math.pow(acceleration.y, 2) +
+        Math.pow(acceleration.z, 2)
+      );
+
+      // Detect a step based on the adjusted acceleration magnitude threshold
+      if (accelerationMagnitude > stepThreshold) {
+        stepCount++;
+        updateStepCountDisplay();
+        updateCaloriesBurned();
+      }
+    }
+  });
+
+  window.addEventListener("beforeunload", () => {
+    saveStepCountAndCaloriesToLocalStorage(convertStepsToCalories(stepCount, gender, height, age, weight));
+  });
+
+  // Function to update the step count and calories burned in the 'program_members' table
+  function updateProgramMemberData(userReference, sessionCode, stepCount, caloriesBurned) {
+    // Prepare the data to send
+    const data = {
+      userReference: userReference,
+      sessionCode: sessionCode,
+      stepCount: stepCount,
+      caloriesBurned: caloriesBurned
+    };
+
+    // Make an AJAX request to the server-side PHP script
+    fetch('update_step_count.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result.message);
+      })
+      .catch(error => {
+        console.error('Error updating program member data:', error);
+      });
+  }
+
+  function insertProgramMember(userReference, stepFitReference, sessionCode, timestamp) {
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+
+    // Prepare the data to be sent in the POST request
+    var data = {
+      userReference: userReference,
+      stepFitReference: stepFitReference,
+      sessionCode: sessionCode,
+      timestamp: timestamp
+    };
+
+    // Set the request URL and method
+    xhr.open('POST', 'save_step_count.php', true);
+
+    // Set the request header
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    // Define the onload callback function
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // Handle the successful response from the server
+        console.log(xhr.responseText);
+      } else {
+        // Handle the error response from the server
+        console.error('Error inserting program member:', xhr.status);
+      }
+    };
+
+    // Convert the data to JSON format and send the request
+    xhr.send(JSON.stringify(data));
+  }
+
+  function convertStepsToCalories(stepCount, gender, height, age, weight) {
+    // Conversion formula for calories burned based on step count
+    // Adjust the values and formula according to your specific calculation
+    const caloriesPerStep = 0.05; // Calories burned per step
+    const strideLength = calculateStrideLength(height, gender); // Calculate stride length based on height and gender
+    const distanceInKm = (stepCount * strideLength) / 100000; // Convert stride length and step count to kilometers
+    const caloriesBurned = distanceInKm * caloriesPerStep * weight; // Calculate calories burned
+
+    return caloriesBurned;
+  }
+
+  function calculateStrideLength(height, gender) {
+    // Calculation of stride length based on height and gender
+    // Adjust the formula according to your specific calculation
+    let strideLength;
+    if (gender === 'male') {
+      strideLength = height * 0.415;
+    } else if (gender === 'female') {
+      strideLength = height * 0.413;
+    } else {
+      // Default stride length if gender is not specified
+      strideLength = height * 0.414;
+    }
+
+    return strideLength;
+  }
+
+  // Check for previous step count and calories burned when the page loads
+  window.addEventListener("load", () => {
+    const storedData = JSON.parse(localStorage.getItem('stepFitData'));
+    if (storedData && storedData.sessionCode) {
+      stepCount = storedData.stepCount;
+      updateStepCountDisplay();
+      updateCaloriesBurned();
+      startStepFitProgram(); // Start the program if data is available
+    }
+  });
+</script>
